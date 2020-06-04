988c0f211c6b95daf5158b392cafb4287f448898
yin-123 <1665216869@qq.com>
2020-06-05 00:34:47 +0800

    提交springboot_elasticsearch

--- src/test/java/com/usian/test/IndexSearchTest.java
+++ src/test/java/com/usian/test/IndexSearchTest.java
@@ -4,10 +4,13 @@ import org.elasticsearch.action.search.SearchRequest;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.text.Text;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.SearchHits;
 import org.elasticsearch.search.builder.SearchSourceBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightBuilder;
+import org.elasticsearch.search.fetch.subphase.highlight.HighlightField;
 import org.elasticsearch.search.sort.SortOrder;
 import org.junit.After;
 import org.junit.Before;
@@ -19,6 +22,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Map;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -87,5 +91,44 @@ public class IndexSearchTest {
         searchRequest.source(searchSourceBuilder);
         // 执行搜索
         searchResponse = restHighLevelClient.search(searchRequest,RequestOptions.DEFAULT);
+    }
+
+    @Test
+    public void testHighLightQuery() throws Exception {
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.query(QueryBuilders.matchQuery("name", "spring"));
+        //设置高亮
+        HighlightBuilder highlightBuilder = new HighlightBuilder();
+        highlightBuilder.preTags("<font color='red'>");
+        highlightBuilder.postTags("</font>");
+        highlightBuilder.fields().add(new HighlightBuilder.Field("name"));
+        searchSourceBuilder.highlighter(highlightBuilder);
+
+        searchRequest.source(searchSourceBuilder);
+        searchResponse = restHighLevelClient.search(searchRequest,RequestOptions.DEFAULT);
+    }
+
+    @After
+    public void displayHits() {
+        SearchHits searchHits = searchResponse.getHits();
+        long totalHits = searchHits.getTotalHits();
+        System.out.println("共搜索到" + totalHits + "条文档");
+
+        SearchHit[] hits = searchHits.getHits();
+        for (int i = 0; i < hits.length; i++) {
+            SearchHit hit = hits[i];
+            String id = hit.getId();
+            System.out.println("id：" + id);
+            String source = hit.getSourceAsString();
+            System.out.println(source);
+
+            Map<String, HighlightField> highlightFields = hit.getHighlightFields();
+            if (highlightFields != null) {
+                HighlightField highlightField = highlightFields.get("name");
+                Text[] fragments = highlightField.getFragments();
+                System.out.println("高亮字段：" + fragments[0].toString());
+            }
+        }
+
     }
 }
